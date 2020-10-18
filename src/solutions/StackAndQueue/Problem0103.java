8318bfc3438b1d8b575469c631454c3a8e5e9e77
Super262 <virusformail@gmail.com>
2020-10-18 21:23:07 +0800

    Finished Problem0103.

--- src/solutions/StackAndQueue/Problem0103.java
+++ src/solutions/StackAndQueue/Problem0103.java
@@ -1,7 +1,6 @@
 package solutions.StackAndQueue;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem0103 {
 
@@ -14,7 +13,29 @@ public class Problem0103 {
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        boolean isFromLeft = true;
+        if(root != null){
+            queue.add(root);
+        }
+        while(!queue.isEmpty()){
+            ArrayList<Integer> tempResult = new ArrayList<>();
+            for(int i = queue.size(); i > 0; --i){
+                TreeNode t = queue.poll();
+                tempResult.add(t.val);
+                if(t.left != null){
+                    queue.add(t.left);
+                }
+                if(t.right != null){
+                    queue.add(t.right);
+                }
+            }
+            if(!isFromLeft){
+                Collections.reverse(tempResult);
+            }
+            isFromLeft = !isFromLeft;
+            result.add(tempResult);
+        }
         return result;
     }
 }
