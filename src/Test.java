5597758f80f0ef354f910939b1da87a6042ae311
Super262 <virusformail@gmail.com>
2020-12-19 10:26:58 +0800

    Cleaned main().

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,6 @@ import solutions.StackAndQueue.Problem1541;
 
 public class Test {
     public static void main(String[] args){
-        Problem1541 p = new Problem1541();
-        System.out.println(p.minInsertions("(()))"));
+        
     }
 }
