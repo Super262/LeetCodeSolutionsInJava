5bca105b59c889e6eeed9cf5d26f93f3fe37d5ad
Super262 <virusformail@gmail.com>
2020-12-19 15:31:04 +0800

    Bug fixed.

--- src/solutions/StackAndQueue/Problem0735.java
+++ src/solutions/StackAndQueue/Problem0735.java
@@ -4,40 +4,37 @@ import java.util.Stack;
 
 public class Problem0735 {
     public int[] asteroidCollision(int[] asteroids) {
-        if(asteroids.length == 0){
-            return new int[0];
-        }
-        if(asteroids.length == 1){
+        if (asteroids.length == 1 || asteroids.length == 0){
             return asteroids;
         }
-        Stack<Integer> temp = new Stack<>();
-        for(int as : asteroids){
-            if(temp.empty() || !(temp.peek() > 0 && as < 0)){
-                temp.push(as);
+        Stack<Integer> stack = new Stack<>();
+        for (int as : asteroids){
+            boolean comingAsExploded = false;
+            while (!stack.empty() && stack.peek() > 0 && as < 0){
+                int asRight = stack.peek();
+                int asLeft = -as;
+                if (asRight == asLeft){
+                    comingAsExploded = true;
+                    stack.pop();
+                    break;
+                }
+                else if(asRight < asLeft){
+                    stack.pop();
+                    comingAsExploded = false;
+                }
+                else{
+                    comingAsExploded = true;
+                    break;
+                }
             }
-            else{
-                boolean allExploded = false;
-                while (!temp.empty() && (temp.peek() > 0 && as < 0)){
-                    if(Math.abs(temp.peek()) == Math.abs(as)){
-                        temp.pop();
-                        allExploded = true;
-                        break;
-                    }
-                    else{
-                        int p = temp.peek();
-                        temp.pop();
-                        as = Math.abs(p) > Math.abs(as) ? p : as;
-                    }
-                }
-                if(!allExploded){
-                    temp.push(as);
-                }
+            if(!comingAsExploded){
+                stack.push(as);
             }
         }
-        int[] result = new int[temp.size()];
+        int[] result = new int[stack.size()];
         for(int i = result.length - 1; i >= 0; --i){
-            result[i] = temp.pop();
+            result[i] = stack.pop();
         }
-        return  result;
+        return result;
     }
 }
