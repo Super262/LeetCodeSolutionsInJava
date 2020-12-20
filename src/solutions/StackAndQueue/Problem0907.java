9b0821652d8455af1cbf9ab65a0e650fda1093b8
Super262 <virusformail@gmail.com>
2020-12-20 10:57:08 +0800

    Bug fixed.

--- src/solutions/StackAndQueue/Problem0907.java
+++ src/solutions/StackAndQueue/Problem0907.java
@@ -6,36 +6,37 @@ public class Problem0907 {
     public int sumSubarrayMins(int[] A) {
         long sum = 0, base = (long) (Math.pow(10, 9) + 7);
         int[] leftLess = new int[A.length], rightLess = new int[A.length];
-        Stack<Integer> temp = new Stack<>();
-        // 注意两次循环出栈的条件：一次有等于，一次没有 -> 应对特殊情况：序列中有2个数相等。
+        Stack<Integer> temp1 = new Stack<>(), temp2 = new Stack<>();
+
+        // 注意两次循环出栈的条件：一次有等于（>=），一次没有（>）-> 应对特殊情况：序列中有2个数相等。
         // 如果不作处理，这两个相等的数，会使子序列重复
+
         for(int i = 0; i < A.length; ++i){
-            while(!temp.empty() && A[temp.peek()] > A[i]){
-                temp.pop();
+            while(!temp1.empty() && A[temp1.peek()] > A[i]){
+                temp1.pop();
             }
-            if(temp.empty()){
+            if(temp1.empty()){
                 leftLess[i] = -1;
             }
             else{
-                leftLess[i] = temp.peek();
+                leftLess[i] = temp1.peek();
             }
-            temp.push(i);
+            temp1.push(i);
         }
-        temp.clear();
         for(int i = A.length - 1; i >= 0; --i){
-            while(!temp.empty() && A[temp.peek()] >= A[i]){
-                temp.pop();
+            while(!temp2.empty() && A[temp2.peek()] >= A[i]){
+                temp2.pop();
             }
-            if(temp.empty()){
+            if(temp2.empty()){
                 rightLess[i] = A.length;
             }
             else{
-                rightLess[i] = temp.peek();
+                rightLess[i] = temp2.peek();
             }
-            temp.push(i);
+            temp2.push(i);
         }
         for(int i = 0; i < A.length; ++i){
-            sum += (long) A[i] * (rightLess[i] - i) * (i - leftLess[i]);
+            sum += ((long) A[i]) * ((long) rightLess[i] - (long) i) * ((long) i - (long) leftLess[i]);
         }
         return (int) (sum % base);
     }
