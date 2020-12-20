451238e623047906483c1eeca311c795ac4a73c7
Super262 <virusformail@gmail.com>
2020-12-20 15:47:11 +0800

    Finished Problem1124.

--- src/solutions/StackAndQueue/Problem1124.java
+++ src/solutions/StackAndQueue/Problem1124.java
@@ -5,11 +5,11 @@ import java.util.Stack;
 public class Problem1124 {
     public int longestWPI(int[] hours) {
         int result = 0;
-        int[] scores = hours.clone();
+        int[] scores = new int[hours.length];
         int[] presum = new int[hours.length+1];
         Stack<Integer> descStack = new Stack<>();
-        for(int i = 0; i < scores.length; ++i){
-            if(scores[i] < 9){
+        for(int i = 0; i < hours.length; ++i){
+            if(hours[i] < 9){
                 scores[i] = -1;
             }
             else{
@@ -27,16 +27,10 @@ public class Problem1124 {
                 descStack.push(i);
             }
         }
-        for(int i = hours.length; i >= 0; --i){
-            while(!descStack.empty() && descStack.peek() >= i){
-                descStack.pop();
-            }
+        for(int i = hours.length; i >= result; --i){
             while(!descStack.empty() && presum[descStack.peek()] < presum[i]){
                 result = Math.max(result, i - descStack.peek());
                 descStack.pop();
-            }
-            if(descStack.empty()){
-                break;
             }
         }
         return result;
