dbd3aa6849fe5ee908c38684d4c535e52b0cef8e
Super262 <virusformail@gmail.com>
2020-12-20 21:07:32 +0800

    Bugs fixed.

--- src/solutions/StackAndQueue/Problem1249.java
+++ src/solutions/StackAndQueue/Problem1249.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Problem1249 {
     public String minRemoveToMakeValid(String s) {
         ArrayList<Integer> stack = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); ++i){
             switch (s.charAt(i)){
                 case '(':{
@@ -22,6 +21,7 @@ public class Problem1249 {
                 }
             }
         }
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); ++i){
             if (!stack.isEmpty() && i == stack.get(0)){
                 stack.remove(0);
