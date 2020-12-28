d64c9ccdaa2150d198a91e8edf8bae59767f2c21
Super262 <virusformail@gmail.com>
2020-12-28 13:47:04 +0800

    Bugs Fixed.

--- src/solutions/StackAndQueue/Problem0224.java
+++ src/solutions/StackAndQueue/Problem0224.java
@@ -8,13 +8,13 @@ public class Problem0224 {
         final char[] operatorsStack =  new char[strLen];
         int numStackPeek = -1;
         int opStackSize = 0, opStackPeek = -1;
-        boolean prevCharIsNum = false;
+        boolean prevCharIsDigit = false;
         for (int i = 0; i < strLen; ++i){
             char ch = s.charAt(i);
             if(ch == '('){
                 operatorsStack[++opStackPeek] = ch;
                 ++opStackSize;
-                prevCharIsNum = false;
+                prevCharIsDigit = false;
             }
             else if (ch == '+' || ch == '-'){
                 while(opStackSize > 0 && operatorsStack[opStackPeek] != '('){
@@ -27,17 +27,17 @@ public class Problem0224 {
                 }
                 operatorsStack[++opStackPeek] = ch;
                 ++opStackSize;
-                prevCharIsNum = false;
+                prevCharIsDigit = false;
             }
             else if (ch >= '0' && ch <= '9'){
                 int digit = ch - '0';
-                if (prevCharIsNum){
+                if (prevCharIsDigit){
                     numbersStack[numStackPeek] = numbersStack[numStackPeek] * 10 + digit;
                 }
                 else{
                     numbersStack[++numStackPeek] = digit;
                 }
-                prevCharIsNum = true;
+                prevCharIsDigit = true;
             }
             else if (ch == ')'){
                 while(operatorsStack[opStackPeek] != '('){
@@ -50,10 +50,10 @@ public class Problem0224 {
                 }
                 --opStackPeek;
                 --opStackSize;
-                prevCharIsNum = false;
+                prevCharIsDigit = false;
             }
             else{
-                prevCharIsNum = false;
+                prevCharIsDigit = false;
             }
         }
         return numbersStack[numStackPeek];
