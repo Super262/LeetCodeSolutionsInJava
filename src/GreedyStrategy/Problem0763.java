f6abf071252a5d3d15947ee3f7cdc69fdddb636d
Super262 <virusformail@gmail.com>
2021-02-02 23:33:25 +0800

    Finished Problem 763.

---
+++ src/GreedyStrategy/Problem0763.java
@@ -0,0 +1,32 @@
+package GreedyStrategy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem0763 {
+    public List<Integer> partitionLabels(String S) {
+        if(S == null || S.isEmpty()){
+            return new LinkedList<>();
+        }
+        int sLen = S.length();
+        int[] chEnd = new int[26];
+        for(int i = 0; i < sLen; ++i){
+            chEnd[S.charAt(i) - 'a'] = i;
+        }
+        LinkedList<Integer> result = new LinkedList<>();
+        int i = 0;
+        int start = 0;
+        int end;
+        while(i < sLen){
+            end = chEnd[S.charAt(i) - 'a'];
+            ++i;
+            while(i <= end){
+                end = Math.max(end, chEnd[S.charAt(i) - 'a']);
+                ++i;
+            }
+            result.addLast(end - start + 1);
+            start = end + 1;
+        }
+        return result;
+    }
+}
