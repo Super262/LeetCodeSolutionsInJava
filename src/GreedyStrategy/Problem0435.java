cdd2f4aea8952e092075edca3eb9241f7500feac
Super262 <virusformail@gmail.com>
2021-02-02 15:44:40 +0800

    Finished Problem 435.

---
+++ src/GreedyStrategy/Problem0435.java
@@ -0,0 +1,23 @@
+package GreedyStrategy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Problem0435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals,Comparator.comparingInt(o -> o[1]));
+        int result = 0;
+        int[] prev = null;
+        for (int[] current : intervals) {
+            if (prev != null && current[0] < prev[1]) {
+                ++result;
+            } else {
+                prev = current;
+            }
+        }
+        return result;
+    }
+}
