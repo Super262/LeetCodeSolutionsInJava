da8227881b9296f9d7aa9cdd8f0b6ffa2e1046ce
Super262 <virusformail@gmail.com>
2021-02-02 14:42:13 +0800

    Finished Problem 75.

---
+++ src/Sequencing/Problem0075.java
@@ -0,0 +1,24 @@
+package Sequencing;
+
+public class Problem0075 {
+    public void sortColors(int[] nums) {
+        int zeroP = -1;
+        int oneP = 0;
+        int twoP = nums.length;
+        while (oneP < twoP) {
+            if (nums[oneP] == 1) {
+                ++oneP;
+            } else if (nums[oneP] == 2) {
+                swap(nums,oneP,--twoP);
+            } else {
+                swap(nums,oneP++,++zeroP);
+            }
+        }
+    }
+
+    private void swap(int[] nums,int a,int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+}
