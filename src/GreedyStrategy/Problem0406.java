0bdfb7703ddb60caf262426f3c15c3b6a1c7c34f
Super262 <virusformail@gmail.com>
2021-02-02 19:05:30 +0800

    Finished Problem 406.

---
+++ src/GreedyStrategy/Problem0406.java
@@ -0,0 +1,28 @@
+package GreedyStrategy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Problem0406 {
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        Arrays.sort(people,(p1,p2) -> {
+            if (p1[0] == p2[0]) {
+                return Integer.compare(p1[1],p2[1]);
+            } else {
+                if (p1[0] > p2[0]) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        ArrayList<int[]> result = new ArrayList<>(people.length);
+        for(int[] p : people){
+            result.add(p[1], p);
+        }
+        return result.toArray(new int[people.length][]);
+    }
+}
