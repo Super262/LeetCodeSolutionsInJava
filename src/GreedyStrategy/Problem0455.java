1e3a4b679753fdf45a924f0952a17508eab78e82
Super262 <virusformail@gmail.com>
2021-02-02 15:12:43 +0800

    Finished Problem 455.

---
+++ src/GreedyStrategy/Problem0455.java
@@ -0,0 +1,21 @@
+package GreedyStrategy;
+
+import java.util.Arrays;
+
+public class Problem0455 {
+    public int findContentChildren(int[] g,int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gP = 0;
+        int sP = 0;
+        while (gP < g.length && sP < s.length) {
+            if (g[gP] <= s[sP]) {
+                ++sP;
+                ++gP;
+            } else {
+                ++sP;
+            }
+        }
+        return gP;
+    }
+}
