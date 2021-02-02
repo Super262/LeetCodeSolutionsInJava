8f75feb2e1a87cd474180c59653dbc84be9003a9
Super262 <virusformail@gmail.com>
2021-02-02 11:03:51 +0800

    Finished Problem 347.

---
+++ src/Sequencing/Problem0347.java
@@ -0,0 +1,31 @@
+package Sequencing;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Problem0347 {
+    public int[] topKFrequent(int[] nums,int k) {
+        HashMap<Integer, Integer> numToFreq = new HashMap<>();
+        int curFreq;
+        for (int num : nums) {
+            curFreq = 1;
+            if (numToFreq.containsKey(num)) {
+                curFreq += numToFreq.get(num);
+            }
+            numToFreq.put(num,curFreq);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(numToFreq::get));
+        for (int num : numToFreq.keySet()) {
+            if (pq.size() < k) {
+                pq.add(num);
+            } else {
+                if(numToFreq.get(pq.peek()) < numToFreq.get(num)){
+                    pq.poll();
+                    pq.add(num);
+                }
+            }
+        }
+        return pq.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
