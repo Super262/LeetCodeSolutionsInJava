fc8f305bebdff67ad673e5771eb6e0dc3e688e81
Super262 <virusformail@gmail.com>
2021-02-03 17:51:17 +0800

    Revised Problem 34.

--- src/BinarySearch/Problem0034.java
+++ src/BinarySearch/Problem0034.java
@@ -1,28 +1,27 @@
 package BinarySearch;
 
 public class Problem0034 {
-    private int[] searchForRange(int[] nums,int start,int end,int target) {
-        if (start >= end) {
-            return new int[]{-1,-1};
-        }
-        int mid = start + (end - start) / 2;
-        if (nums[mid] == target) {
-            int[] result = new int[]{mid,mid};
-            for (int i = mid + 1; i < end; ++i) {
-                result[1] = nums[i] == target ? i : result[1];
-            }
-            for (int i = mid - 1; i >= start; --i) {
-                result[0] = nums[i] == target ? i : result[0];
-            }
-            return result;
-        } else if (nums[mid] > target) {
-            return searchForRange(nums,start,mid,target);
-        } else {
-            return searchForRange(nums,mid + 1,end,target);
-        }
-    }
-
     public int[] searchRange(int[] nums,int target) {
-        return searchForRange(nums,0,nums.length,target);
+        int[] result = new int[]{-1,-1};
+        int start = 0;
+        int end = nums.length;
+        int mid;
+        while (start < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                for (int i = mid; i < end; ++i) {
+                    result[1] = nums[i] == target ? i : result[1];
+                }
+                for (int i = mid; i >= start; --i) {
+                    result[0] = nums[i] == target ? i : result[0];
+                }
+                return result;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
     }
 }
