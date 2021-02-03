7115b785666235d888d7999c8decd69554e78752
Super262 <virusformail@gmail.com>
2021-02-03 22:10:38 +0800

    Finished Problem 241.

--- src/DivideAndConquer/Problem0241.java
+++ src/DivideAndConquer/Problem0241.java
@@ -11,44 +11,41 @@ public class Problem0241 {
         }
         int sLen = input.length();
         int i = 0;
-        while(i < sLen){
-            if(input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*'){
+        while (i < sLen) {
+            if (!Character.isDigit(input.charAt(i))) {
                 break;
             }
             ++i;
         }
-        if(i >= sLen){
-            result.addLast(Integer.parseInt(input.substring(0, i)));
+        if (i >= sLen) {
+            result.addLast(Integer.parseInt(input.substring(0,i)));
             return result;
         }
-        while(i < sLen){
-            if(input.charAt(i) == '+'){
-                List<Integer> left  = diffWaysToCompute(input.substring(0, i));
-                List<Integer> right = diffWaysToCompute(input.substring(i + 1, sLen));
-                for(int l : left){
-                    for(int r : right){
-                        result.addLast(l + r);
+        while (i < sLen) {
+            if (!Character.isDigit(input.charAt(i))) {
+                List<Integer> left = diffWaysToCompute(input.substring(0,i));
+                List<Integer> right = diffWaysToCompute(input.substring(i + 1,sLen));
+                if (input.charAt(i) == '+') {
+                    for (int l : left) {
+                        for (int r : right) {
+                            result.addLast(l + r);
+                        }
                     }
-                }
-            } else if (input.charAt(i) == '-'){
-                List<Integer> left  = diffWaysToCompute(input.substring(0, i));
-                List<Integer> right = diffWaysToCompute(input.substring(i + 1, sLen));
-                for(int l : left){
-                    for(int r : right){
-                        result.addLast(l - r);
+                } else if (input.charAt(i) == '-') {
+                    for (int l : left) {
+                        for (int r : right) {
+                            result.addLast(l - r);
+                        }
                     }
-                }
-            } else if (input.charAt(i) == '*'){
-                List<Integer> left  = diffWaysToCompute(input.substring(0, i));
-                List<Integer> right = diffWaysToCompute(input.substring(i + 1, sLen));
-                for(int l : left){
-                    for(int r : right){
-                        result.addLast(l * r);
+                } else {
+                    for (int l : left) {
+                        for (int r : right) {
+                            result.addLast(l * r);
+                        }
                     }
                 }
             }
             ++i;
-
         }
         return result;
     }
