f6e2dfd8102c0d4c3ba4944dc05c79d190f0712c
Super262 <virusformail@gmail.com>
2021-02-06 14:17:28 +0800

    Finished Problem 70.

---
+++ src/DynamicProgramming/Problem0070.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+public class Problem0070 {
+    public int climbStairs(int n) {
+        if (n < 3) {
+            return n;
+        }
+        int f1 = 1;
+        int f2 = 2;
+        int fn = 0;
+        n = n - 2;
+        while (n > 0) {
+            fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+            --n;
+        }
+        return fn;
+    }
+}
