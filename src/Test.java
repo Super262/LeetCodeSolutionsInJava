d98773960fcc68157d9f3057f1e54d6671423b47
Super262 <virusformail@gmail.com>
2021-02-08 14:58:07 +0800

    Finished Problem 583.

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,4 @@
-import DynamicProgramming.LongestSubsequence.Problem0300;
-
 public class Test {
     public static void main(String[] args) {
-        Problem0300 p = new Problem0300();
-        p.lengthOfLIS(new int[]{0});
     }
 }
