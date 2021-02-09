233adabc4b6dae08df29dcf4b5e3db225b9b9039
Super262 <virusformail@gmail.com>
2021-02-09 23:22:22 +0800

    Revised Problem 540.

--- src/BinarySearch/Problem0540.java
+++ src/BinarySearch/Problem0540.java
@@ -7,29 +7,23 @@ public class Problem0540 {
         }
         int low = 0;
         int high = nums.length - 1;
-        int mid, midL, midR;
-        while (low <= high) {
+        int mid;
+        while (low < high) {
             mid = low + (high - low) / 2;
-            midL = mid - 1 >= 0 ? nums[mid - 1] : nums[0] - 1;
-            midR = mid + 1 < nums.length ? nums[mid + 1] : nums[nums.length - 1] + 1;
-            if (nums[mid] != midL && nums[mid] != midR) {
-                return nums[mid];
+            if (mid % 2 == 0) {
+                if (mid > 0 && nums[mid - 1] == nums[mid]) {
+                    high = mid;
+                } else {
+                    low = mid;
+                }
             } else {
-                if (nums[mid] == midL) {
-                    if ((mid - low + 1) % 2 == 0) {
-                        low = mid + 1;
-                    } else {
-                        high = mid;
-                    }
-                } else if (nums[mid] == midR) {
-                    if ((high - mid + 1) % 2 == 0) {
-                        high = mid - 1;
-                    } else {
-                        low = mid;
-                    }
+                if (mid > 0 && nums[mid - 1] == nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
         }
-        return nums[0];
+        return nums[low];
     }
 }
