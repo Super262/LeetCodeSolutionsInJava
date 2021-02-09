73b8119583210932da3e73267c003b65b6ad9343
Super262 <virusformail@gmail.com>
2021-02-09 23:40:25 +0800

    Revised Problem 278.

--- src/BinarySearch/Problem0278.java
+++ src/BinarySearch/Problem0278.java
@@ -6,24 +6,17 @@ public class Problem0278 {
     }
 
     public int firstBadVersion(int n) {
-        if (n == 1) {
-            return 1;
-        }
         int low = 1;
         int high = n;
         int mid;
-        while (low <= high) {
+        while (low < high) {
             mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
-                if (mid == low || !isBadVersion(mid - 1)) {
-                    return mid;
-                } else {
-                    high = mid - 1;
-                }
+                high = mid;
             } else {
                 low = mid + 1;
             }
         }
-        return n;
+        return low;
     }
 }
