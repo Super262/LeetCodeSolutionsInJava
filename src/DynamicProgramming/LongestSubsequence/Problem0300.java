029d5a4ec4befcee99cc59dafae6c6872adebb5a
Super262 <virusformail@gmail.com>
2021-02-14 18:22:09 +0800

    Revised Problem 300.

--- src/DynamicProgramming/LongestSubsequence/Problem0300.java
+++ src/DynamicProgramming/LongestSubsequence/Problem0300.java
@@ -5,20 +5,34 @@ public class Problem0300 {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
-        int preMax;
-        int result = 1;
+        int currentLen = 1;
+        int[] tails = new int[nums.length];
+        tails[0] = nums[0];
+        int tempIndex;
         for (int i = 1; i < nums.length; ++i) {
-            preMax = 0;
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i]) {
-                    preMax = Math.max(preMax,dp[j]);
-                }
+            tempIndex = findPreMax(tails,currentLen,nums[i]);
+            tails[tempIndex] = nums[i];
+            if (tempIndex == currentLen) {
+                ++currentLen;
             }
-            dp[i] = preMax + 1;
-            result = Math.max(result,dp[i]);
         }
-        return result;
+        return currentLen;
+    }
+
+    private int findPreMax(int[] nums,final int end,final int key) {
+        int low = 0;
+        int high = end;
+        int mid;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == key) {
+                return mid;
+            } else if (nums[mid] > key) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 }
