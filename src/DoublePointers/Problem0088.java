7f552abaee19d43e17f39a4ff91c98d39a1402f3
Super262 <virusformail@gmail.com>
2021-02-16 05:40:13 +0800

    Revised Problem 88.

--- src/DoublePointers/Problem0088.java
+++ src/DoublePointers/Problem0088.java
@@ -2,19 +2,21 @@ package DoublePointers;
 
 public class Problem0088 {
     public void merge(int[] nums1,int m,int[] nums2,int n) {
-        // two get pointers for nums1 and nums2
-        int p1 = m - 1;
-        int p2 = n - 1;
-        // set pointer for nums1
-        int p = m + n - 1;
-
-        // while there are still elements to compare
-        while ((p1 >= 0) && (p2 >= 0))
-            // compare two elements from nums1 and nums2
-            // and add the largest one in nums1
-            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
-
-        // add missing elements from nums2
-        System.arraycopy(nums2,0,nums1,0,p2 + 1);
+        int resultTop = nums1.length-1;
+        --m;
+        --n;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]){
+                nums1[resultTop--] = nums1[m--];
+            } else{
+                nums1[resultTop--] = nums2[n--];
+            }
+        }
+        while (m >= 0) {
+            nums1[resultTop--] = nums1[m--];
+        }
+        while (n >= 0) {
+            nums1[resultTop--] = nums2[n--];
+        }
     }
 }
