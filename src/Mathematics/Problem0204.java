10d70134f7401d871f5af331ed2449e3f2f9516b
Super262 <virusformail@gmail.com>
2021-02-16 20:09:38 +0800

    Finished Problem 204.

---
+++ src/Mathematics/Problem0204.java
@@ -0,0 +1,24 @@
+package Mathematics;
+
+import java.util.Arrays;
+
+public class Problem0204 {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime,true);
+        int result = 0;
+        long temp;
+        for (int num = 2; num < n; ++num) {
+            if (isPrime[num]) {
+                ++result;
+                temp = ((long) num) * ((long) num);
+                if (temp < n) {
+                    for (long j = temp; j < n; j += num) {
+                        isPrime[(int) j] = false;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
