8d52c448e2d475d5ec6e67b500fff5b1082090e2
Super262 <virusformail@gmail.com>
2021-02-20 05:45:27 +0800

    Finished Problem 160.

---
+++ src/DataStructure/LinkedList/Problem0160.java
@@ -0,0 +1,32 @@
+package DataStructure.LinkedList;
+
+public class Problem0160 {
+    public ListNode getIntersectionNode(ListNode headA,ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+            if (p1 == null && p2 == null) {
+                return null;
+            } else {
+                p1 = p1 == null ? headB : p1;
+                p2 = p2 == null ? headA : p2;
+            }
+        }
+        return p1;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
