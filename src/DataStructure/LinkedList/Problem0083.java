108b339212f8e130d524408a5b5daed9ee053692
Super262 <virusformail@gmail.com>
2021-02-27 08:56:08 +0800

    Finished Problem 83.

---
+++ src/DataStructure/LinkedList/Problem0083.java
@@ -0,0 +1,20 @@
+package DataStructure.LinkedList;
+
+public class Problem0083 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2.next != null) {
+            p2 = p2.next;
+            if (p1.val != p2.val) {
+                p1.next = p2;
+                p1 = p2;
+            }
+        }
+        p1.next = null;
+        return head;
+    }
+}
