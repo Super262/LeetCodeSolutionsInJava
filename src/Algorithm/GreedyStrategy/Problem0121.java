a94711025a61933198a27751acc98475555ea938
Super262 <virusformail@gmail.com>
2021-03-16 10:51:19 +0800

    Finished Problem 121.

---
+++ src/Algorithm/GreedyStrategy/Problem0121.java
@@ -0,0 +1,18 @@
+package Algorithm.GreedyStrategy;
+
+public class Problem0121 {
+    public int maxProfit(int[] prices) {
+        if(prices.length < 2){
+            return 0;
+        }
+        int prevMin = prices[0];
+        int maxValue = 0;
+
+        for(int price : prices){
+            prevMin = Math.min(price, prevMin);
+            maxValue = Math.max(maxValue, price - prevMin);
+        }
+
+        return maxValue;
+    }
+}
