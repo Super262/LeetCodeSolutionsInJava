e5960df565ea3d6bf2559551e0ff839d99d18bde
Super262 <virusformail@gmail.com>
2021-03-19 02:06:04 +0800

    Finished Problem 321.

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,10 @@
+import Algorithm.GreedyStrategy.Problem0321;
+
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
+        Problem0321 p = new Problem0321();
+        System.out.println(Arrays.toString(p.maxNumber(new int[]{6,7},new int[]{6,0,4},5)));
     }
 }
