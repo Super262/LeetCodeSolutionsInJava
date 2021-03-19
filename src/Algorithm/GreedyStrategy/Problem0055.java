5eade49f3260a9b1d9d0a4200bb7ef44e6eb9a8c
Super262 <virusformail@gmail.com>
2021-03-19 14:06:01 +0800

    Finished Problem 55.

--- src/Algorithm/GreedyStrategy/Problem0055.java
+++ src/Algorithm/GreedyStrategy/Problem0055.java
@@ -6,26 +6,26 @@ public class Problem0055 {
             return false;
         }
         int start = 0;
-        int current_bound;
-        int next_start;
+        int currentBound;
+        int nextStart;
         while (start < nums.length) {
-            current_bound = start + nums[start];
+            currentBound = start + nums[start];
 
             // 不能改变下面的两个if语句块的顺序，应对特殊情况：nums = [1]
-            if (current_bound >= nums.length - 1) {
+            if (currentBound >= nums.length - 1) {
                 return true;
             }
-            if (current_bound <= start) {
+            if (currentBound <= start) {
                 return false;
             }
 
-            next_start = start + 1;
-            for (int candidate = start + 2; candidate <= current_bound; ++candidate) {
-                if (candidate + nums[candidate] >= next_start + nums[next_start]) {
-                    next_start = candidate;
+            nextStart = start + 1;
+            for (int candidate = start + 2; candidate <= currentBound; ++candidate) {
+                if (candidate + nums[candidate] >= nextStart + nums[nextStart]) {
+                    nextStart = candidate;
                 }
             }
-            start = next_start;
+            start = nextStart;
         }
         return false;
     }
