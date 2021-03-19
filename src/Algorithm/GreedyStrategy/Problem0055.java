9681270633485d6f6fe44161d4bf536bf46433fb
Super262 <virusformail@gmail.com>
2021-03-19 13:39:47 +0800

    Finished Problem 55.

---
+++ src/Algorithm/GreedyStrategy/Problem0055.java
@@ -0,0 +1,29 @@
+package Algorithm.GreedyStrategy;
+
+public class Problem0055 {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int current_bound;
+        int next_start;
+        while (start < nums.length) {
+            current_bound = start + nums[start];
+            if (current_bound >= nums.length - 1) {
+                return true;
+            }
+            if (current_bound <= start) {
+                return false;
+            }
+            next_start = start + 1;
+            for (int candidate = start + 2; candidate <= current_bound; ++candidate) {
+                if (candidate + nums[candidate] >= next_start + nums[next_start]) {
+                    next_start = candidate;
+                }
+            }
+            start = next_start;
+        }
+        return false;
+    }
+}
