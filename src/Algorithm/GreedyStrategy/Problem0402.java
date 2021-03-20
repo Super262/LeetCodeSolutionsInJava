cbb1facbfab5df74d8b514da999e24e50ff9a859
Super262 <virusformail@gmail.com>
2021-03-20 09:36:03 +0800

    Finished Problem 402.

--- src/Algorithm/GreedyStrategy/Problem0402.java
+++ src/Algorithm/GreedyStrategy/Problem0402.java
@@ -9,24 +9,28 @@ public class Problem0402 {
         if (k >= numLen) {
             return "0";
         }
-        int deletedCount = k;
+        int deletedCount = 0;
         char[] stack = new char[numLen];
         int stackTop = -1;
         char digit;
         for (int i = 0; i < numLen; ++i) {
             digit = num.charAt(i);
-            while (stackTop >= 0 && deletedCount > 0 && stack[stackTop] > digit) {
+            while (stackTop >= 0 && deletedCount < k && stack[stackTop] > digit) {
                 --stackTop;
-                --deletedCount;
+                ++deletedCount;
             }
             if (digit == '0' && stackTop == -1) {
                 continue;
             }
             stack[++stackTop] = digit;
         }
+        while(deletedCount < k){
+            ++deletedCount;
+            --stackTop;
+        }
         if (stackTop == -1) {
             return "0";
         }
-        return String.valueOf(stack,0,Math.min(numLen - k,stackTop + 1));
+        return String.valueOf(stack,0,stackTop + 1);
     }
 }
