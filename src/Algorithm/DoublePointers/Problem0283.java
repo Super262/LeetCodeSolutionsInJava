8cc061b0b48c7b1cf64c0a622d8a9e719706f09c
Super262 <virusformail@gmail.com>
2021-03-23 21:14:52 +0800

    Finished Problem 283.

---
+++ src/Algorithm/DoublePointers/Problem0283.java
@@ -0,0 +1,22 @@
+package Algorithm.DoublePointers;
+
+public class Problem0283 {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                ++left;
+            }
+            ++right;
+        }
+        while (left < nums.length) {
+            nums[left] = 0;
+            ++left;
+        }
+    }
+}
