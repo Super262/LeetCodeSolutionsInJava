8b95de24fb346f878750374bddb598dc08353a2b
Super262 <virusformail@gmail.com>
2021-04-01 10:55:27 +0800

    Cleaned code.

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,5 @@
-import Algorithm.SearchMethods.BFS.Problem0269;
-
 public class Test {
     public static void main(String[] args) {
-        Problem0269 p = new Problem0269();
-        System.out.println(p.alienOrder(new String[]{"wrt","wrf","er","ett","rftt"}));
+
     }
 }
