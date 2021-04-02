beed37cc0646805891843d6896396a4f6cf1378d
Super262 <virusformail@gmail.com>
2021-04-02 21:29:22 +0800

    Finished Problem 1644.

---
+++ src/Algorithm/DivideAndConquer/Problem1644.java
@@ -0,0 +1,41 @@
+package Algorithm.DivideAndConquer;
+
+import java.util.ArrayList;
+
+public class Problem1644 {
+    public TreeNode lowestCommonAncestor(TreeNode root,TreeNode p,TreeNode q) {
+        ArrayList<TreeNode> pPath = new ArrayList<>();
+        ArrayList<TreeNode> qPath = new ArrayList<>();
+        getPath(root,p,pPath);
+        getPath(root,q,qPath);
+        if (pPath.isEmpty() || qPath.isEmpty()) {
+            return null;
+        }
+        int i = 0;
+        while (i < pPath.size() && i < qPath.size()) {
+            if (pPath.get(i) != qPath.get(i)) {
+                break;
+            }
+            ++i;
+        }
+        return pPath.get(i - 1);
+    }
+
+    private boolean getPath(TreeNode root,TreeNode target,ArrayList<TreeNode> path) {
+        if (root == null || target == null) {
+            return false;
+        }
+        path.add(root);
+        if (root == target) {
+            return true;
+        }
+        if (getPath(root.left,target,path)) {
+            return true;
+        }
+        if (getPath(root.right,target,path)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+}
