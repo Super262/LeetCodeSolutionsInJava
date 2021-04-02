13c1064508f634d95377975464018f18385e01a0
Super262 <virusformail@gmail.com>
2021-04-02 18:59:05 +0800

    Finished Problem 1120.

---
+++ src/Algorithm/SearchMethods/BFS/Problem1120.java
@@ -0,0 +1,32 @@
+package Algorithm.SearchMethods.BFS;
+
+public class Problem1120 {
+    public double maximumAverageSubtree(TreeNode root) {
+        return helper(root).avgMax;
+    }
+
+    private ResultPack helper(TreeNode root) {
+        if (root == null) {
+            return new ResultPack(0,0,Double.MIN_VALUE);
+        }
+        ResultPack leftResult = helper(root.left);
+        ResultPack rightResult = helper(root.right);
+        int rootSum = root.val + leftResult.valuesSum + rightResult.valuesSum;
+        int nodesCount = 1 + leftResult.nodesCount + rightResult.nodesCount;
+        double rootAvg = (double) rootSum / nodesCount;
+        double avgMax = Math.max(rootAvg,Math.max(rightResult.avgMax,leftResult.avgMax));
+        return new ResultPack(nodesCount,rootSum,avgMax);
+    }
+
+    private static class ResultPack {
+        int nodesCount;
+        int valuesSum;
+        double avgMax;
+
+        ResultPack(int nodesCount,int valuesSum,double avgMax) {
+            this.avgMax = avgMax;
+            this.nodesCount = nodesCount;
+            this.valuesSum = valuesSum;
+        }
+    }
+}
---
+++ src/Algorithm/SearchMethods/BFS/TreeNode.java
@@ -0,0 +1,11 @@
+package Algorithm.SearchMethods.BFS;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
