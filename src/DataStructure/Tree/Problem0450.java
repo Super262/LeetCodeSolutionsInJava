5d50d36689256199e0f7e3d9ca721da85f86788a
Super262 <virusformail@gmail.com>
2021-04-03 22:33:29 +0800

    Finished Problem 450.

---
+++ src/DataStructure/Tree/Problem0450.java
@@ -0,0 +1,36 @@
+package DataStructure.Tree;
+
+public class Problem0450 {
+    public TreeNode deleteNode(TreeNode root,int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.right == null && root.left == null) {
+                root = null;
+            } else if (root.right != null && root.left != null) {
+                root.val = findMax(root.left);
+                root.left = deleteNode(root.left,root.val);
+            } else if (root.left != null) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left,key);
+        } else {
+            root.right = deleteNode(root.right,key);
+        }
+        return root;
+    }
+
+    private int findMax(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        if (root.right == null) {
+            return root.val;
+        }
+        return findMax(root.right);
+    }
+}
