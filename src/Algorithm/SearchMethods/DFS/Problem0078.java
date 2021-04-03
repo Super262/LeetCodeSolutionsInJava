b013d972e818951ed1913e7e4c4187841a2470bf
Super262 <virusformail@gmail.com>
2021-04-03 21:50:20 +0800

    Finished Problem 78.

--- src/Algorithm/SearchMethods/DFS/Problem0078.java
+++ src/Algorithm/SearchMethods/DFS/Problem0078.java
@@ -1,47 +1,28 @@
 package Algorithm.SearchMethods.DFS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem0078 {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new LinkedList<>();
-        for (int i = 0; i <= nums.length; ++i) {
-            result.addAll(getCombinations(nums,0,i));
-        }
-        return result;
+        int[] numsCopied = new int[nums.length];
+        System.arraycopy(nums,0,numsCopied,0,nums.length);
+        Arrays.sort(numsCopied);
+        List<List<Integer>> results = new LinkedList<>();
+        helper(numsCopied,0,new ArrayList<>(numsCopied.length),results);
+        return results;
     }
 
-    private List<List<Integer>> getCombinations(int[] nums,int start,int targetLength) {
-        List<List<Integer>> result = new LinkedList<>();
-        if (targetLength < 0 || targetLength > nums.length - start) {
-            return result;
-        } else if (targetLength == 0) {
-            LinkedList<Integer> tempResult = new LinkedList<>();
-            result.add(tempResult);
-        } else if (targetLength == nums.length - start) {
-            LinkedList<Integer> tempResult = new LinkedList<>();
-            for (int i = start; i < nums.length; ++i) {
-                tempResult.addLast(nums[i]);
-            }
-            result.add(tempResult);
-        } else {
-            if (targetLength == 1) {
-                for (int i = start; i < nums.length; ++i) {
-                    LinkedList<Integer> tempResult = new LinkedList<>();
-                    tempResult.addLast(nums[i]);
-                    result.add(tempResult);
-                }
-            } else {
-                for (int i = start; i < nums.length; ++i) {
-                    List<List<Integer>> postPart = getCombinations(nums,i + 1,targetLength - 1);
-                    for (List<Integer> l : postPart) {
-                        l.add(0,nums[i]);
-                    }
-                    result.addAll(postPart);
-                }
-            }
+    private void helper(int[] nums,int index,List<Integer> subset,List<List<Integer>> results) {
+        if (index >= nums.length) {
+            results.add(new ArrayList<>(subset));
+            return;
         }
-        return result;
+        subset.add(nums[index]);
+        helper(nums,index + 1,subset,results);
+        subset.remove(subset.size() - 1);
+        helper(nums,index + 1,subset,results);
     }
 }
