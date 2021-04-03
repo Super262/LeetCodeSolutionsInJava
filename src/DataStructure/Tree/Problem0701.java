e88a12df9ad7f0cb56409cb1e502bf1df01c64be
Super262 <virusformail@gmail.com>
2021-04-03 14:29:35 +0800

    Finished Problem 701.

---
+++ src/DataStructure/Tree/Problem0701.java
@@ -0,0 +1,28 @@
+package DataStructure.Tree;
+
+public class Problem0701 {
+    public TreeNode insertIntoBST(TreeNode root,int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode currentPosition = root;
+        while (true) {
+            if (currentPosition.val == val) {
+                break;
+            } else if (currentPosition.val > val) {
+                if (currentPosition.left == null) {
+                    currentPosition.left = new TreeNode(val);
+                    break;
+                }
+                currentPosition = currentPosition.left;
+            } else {
+                if (currentPosition.right == null) {
+                    currentPosition.right = new TreeNode(val);
+                    break;
+                }
+                currentPosition = currentPosition.right;
+            }
+        }
+        return root;
+    }
+}
