7cc7cf2fe1466547d926c5a8861e0d78728ca324
Super262 <virusformail@gmail.com>
2021-04-05 10:07:06 +0800

    Finished Problem 39.

--- src/Algorithm/SearchMethods/DFS/Problem0039.java
+++ src/Algorithm/SearchMethods/DFS/Problem0039.java
@@ -1,47 +1,35 @@
 package Algorithm.SearchMethods.DFS;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem0039 {
     public List<List<Integer>> combinationSum(int[] candidates,int target) {
-        Arrays.sort(candidates);
-        return getCombinations(candidates,0,target);
+        int[] candidatesUnique = new int[candidates.length];
+        System.arraycopy(candidates,0,candidatesUnique,0,candidates.length);
+        Arrays.sort(candidatesUnique);
+        List<List<Integer>> results = new LinkedList<>();
+        dfs(candidatesUnique,0,target,new ArrayList<>(),results);
+        return results;
     }
 
-    private List<List<Integer>> getCombinations(int[] candidates,final int start,final int target) {
-        List<List<Integer>> result = new LinkedList<>();
-        if (start < candidates.length && target >= candidates[start]) {
-            final int maxCount = target / candidates[start];
-            // count = 0
-            result.addAll(getCombinations(candidates,start + 1,target));
-
-            // count > 0
-            int prevSum;
-            for (int count = 1; count <= maxCount; ++count) {
-                prevSum = count * candidates[start];
-                if (prevSum < target) {
-                    List<List<Integer>> postPart = getCombinations(candidates,start + 1,target - prevSum);
-                    if (!postPart.isEmpty()) {
-                        List<Integer> prefix = new LinkedList<>();
-                        for (int k = 0; k < count; ++k) {
-                            prefix.add(0,candidates[start]);
-                        }
-                        for (List<Integer> postfix : postPart) {
-                            postfix.addAll(0,prefix);
-                            result.add(postfix);
-                        }
-                    }
-                } else {
-                    List<Integer> temp = new LinkedList<>();
-                    for (int k = 0; k < count; ++k) {
-                        temp.add(candidates[start]);
-                    }
-                    result.add(temp);
-                }
-            }
+    private void dfs(int[] candidates,int startIndex,final int target,ArrayList<Integer> combination,List<List<Integer>> results) {
+        if (target < 0) {
+            return;
         }
-        return result;
+        if (target == 0) {
+            results.add(new ArrayList<>(combination));
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; ++i) {
+            if (candidates[i] > target) {
+                return;
+            }
+            combination.add(candidates[i]);
+            dfs(candidates,i,target - candidates[i],combination,results);
+            combination.remove(combination.size() - 1);
+        }
     }
 }
