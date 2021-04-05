32c65eb6abb91a617745d2e2eeb268c6db30331c
Super262 <virusformail@gmail.com>
2021-04-05 14:42:10 +0800

    Finished Problem 212.

--- src/DataStructure/Trie/Problem0212.java
+++ src/DataStructure/Trie/Problem0212.java
@@ -1,4 +1,83 @@
 package DataStructure.Trie;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Problem0212 {
+    public List<String> findWords(char[][] board,String[] words) {
+        Trie root = buildTrie(words);
+        int[][] directions = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        List<String> results = new LinkedList<>();
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int y = 0; y < board.length; ++y) {
+            for (int x = 0; x < board[0].length; ++x) {
+                Trie currentNode = root.children[board[y][x] - 'a'];
+                if (currentNode != null) {
+                    visited[y][x] = true;
+                    dfsToSearch(currentNode,board,visited,y,x,directions,results);
+                    visited[y][x] = false;
+                }
+            }
+        }
+        return results;
+    }
+
+    private Trie buildTrie(String[] words) {
+        if (words == null || words.length == 0) {
+            return new Trie();
+        }
+        Trie root = new Trie();
+        for (String w : words) {
+            addWord(root,w);
+        }
+        return root;
+    }
+
+    private void addWord(Trie root,String word) {
+        if (root == null || word == null || word.isEmpty()) {
+            return;
+        }
+        Trie currentNode = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            if (currentNode.children[ch - 'a'] == null) {
+                currentNode.children[ch - 'a'] = new Trie();
+            }
+            currentNode = currentNode.children[ch - 'a'];
+        }
+        currentNode.isWord = true;
+        currentNode.wordValue = word;
+    }
+
+    private void dfsToSearch(Trie root,char[][] board,boolean[][] visited,int startY,int startX,int[][] directions,List<String> results) {
+        if (root == null) {
+            return;
+        }
+        if (root.isWord) {
+            results.add(root.wordValue);
+            root.isWord = false;  // 避免产生重复的搜索结果
+        }
+        for (int[] d : directions) {
+            int nextY = startY + d[0];
+            int nextX = startX + d[1];
+            if (nextY < 0 || nextY >= board.length || nextX < 0 || nextX >= board[0].length || visited[nextY][nextX]) {
+                continue;
+            }
+            visited[nextY][nextX] = true;
+            dfsToSearch(root.children[board[nextY][nextX] - 'a'],board,visited,nextY,nextX,directions,results);
+            visited[nextY][nextX] = false;
+        }
+    }
+
+    private static class Trie {
+        public boolean isWord;
+        public Trie[] children;
+        public String wordValue;
+
+        public Trie() {
+            this.isWord = false;
+            this.children = new Trie[26];
+            wordValue = null;
+        }
+    }
 }
