1d936436137b8bfbcbda02ff8aaf030f2a316678
Super262 <virusformail@gmail.com>
2021-04-06 19:58:42 +0800

    Finished Problem 138.

---
+++ src/DataStructure/HashTable/Problem0138.java
@@ -0,0 +1,28 @@
+package DataStructure.HashTable;
+
+import java.util.HashMap;
+
+public class Problem0138 {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> oldToNew = new HashMap<>();
+        Node currentNode = head;
+        Node prevNode = null;
+        while (currentNode != null) {
+            Node currentCopied = new Node(currentNode.val);
+            oldToNew.put(currentNode,currentCopied);
+            if (prevNode != null) {
+                prevNode.next = currentCopied;
+            }
+            prevNode = currentCopied;
+            currentNode = currentNode.next;
+        }
+        currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.random != null) {
+                oldToNew.get(currentNode).random = oldToNew.get(currentNode.random);
+            }
+            currentNode = currentNode.next;
+        }
+        return oldToNew.getOrDefault(head,null);
+    }
+}
