91dd54be6c7372072613df894d3415341e7aa8a4
Super262 <virusformail@gmail.com>
2021-04-08 00:18:30 +0800

    Finished Problem 120.

--- src/Algorithm/DynamicProgramming/Partition/Problem0120.java
+++ src/Algorithm/DynamicProgramming/Partition/Problem0120.java
@@ -4,24 +4,19 @@ import java.util.List;
 
 public class Problem0120 {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] memo = new int[triangle.size()][triangle.get(triangle.size() - 1).size()];
-        for (int y = 0; y < memo.length; ++y) {
-            for (int x = 0; x < memo[0].length; ++x) {
-                memo[y][x] = -1;
+        final int N = triangle.size();
+        int[][] dp = new int[N][];
+        for (int i = 0; i < N; ++i) {
+            dp[i] = new int[i + 1];
+        }
+        for (int i = 0; i < N; ++i) {
+            dp[N - 1][i] = triangle.get(N - 1).get(i);
+        }
+        for (int i = N - 2; i >= 0; --i) {
+            for (int j = 0; j < dp[i].length; ++j) {
+                dp[i][j] = Math.min(dp[i + 1][j],dp[i + 1][j + 1]) + triangle.get(i).get(j);
             }
         }
-        return dpToGetSum(triangle,0,0,memo);
-    }
-
-    private int dpToGetSum(List<List<Integer>> triangle,int y,int x,int[][] memo) {
-        if (y == triangle.size()) {
-            return 0;
-        }
-        if (memo[y][x] != -1) {
-            return memo[y][x];
-        }
-        int currentSum = triangle.get(y).get(x) + Math.min(dpToGetSum(triangle,y + 1,x,memo),dpToGetSum(triangle,y + 1,x + 1,memo));
-        memo[y][x] = currentSum;
-        return currentSum;
+        return dp[0][0];
     }
 }
