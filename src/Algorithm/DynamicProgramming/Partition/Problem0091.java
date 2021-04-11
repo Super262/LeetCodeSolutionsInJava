854df2abd46aab10be479edfc4e68c349c97c072
Super262 <virusformail@gmail.com>
2021-04-11 08:37:47 +0800

    Finished Problem 91.

--- src/Algorithm/DynamicProgramming/Partition/Problem0091.java
+++ src/Algorithm/DynamicProgramming/Partition/Problem0091.java
@@ -9,19 +9,22 @@ public class Problem0091 {
         if (sLen == 0) {
             return 1;
         }
-        int[] dp = new int[sLen + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        int f0 = 1;
+        int f1 = s.charAt(0) == '0' ? 0 : 1;
+        int f2 = 0;
         for (int l = 2; l <= sLen; ++l) {
             if (s.charAt(l - 1) != '0') {
-                dp[l] += dp[l - 1];
+                f2 += f1;
             }
             if (s.charAt(l - 2) != '0') {
                 if ((s.charAt(l - 2) - '0') * 10 + (s.charAt(l - 1) - '0') < 27) {
-                    dp[l] += dp[l - 2];
+                    f2 += f0;
                 }
             }
+            f0 = f1;
+            f1 = f2;
+            f2 = 0;
         }
-        return dp[sLen];
+        return f1;
     }
 }
