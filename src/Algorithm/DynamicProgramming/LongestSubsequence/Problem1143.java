efc67d74979606671b31e53381f55f91f10fbf4c
Super262 <virusformail@gmail.com>
2021-04-12 00:09:50 +0800

    Finished Problem 1143.

--- src/Algorithm/DynamicProgramming/LongestSubsequence/Problem1143.java
+++ src/Algorithm/DynamicProgramming/LongestSubsequence/Problem1143.java
@@ -7,16 +7,18 @@ public class Problem1143 {
         }
         int sLen1 = text1.length();
         int sLen2 = text2.length();
-        int[][] dp = new int[sLen1 + 1][sLen2 + 1];
+        int[][] dp = new int[2][sLen2 + 1];
         for (int l1 = 1; l1 <= sLen1; ++l1) {
+            int newL1 = l1 % 2;
+            int oldL1 = (l1 - 1) % 2;
             for (int l2 = 1; l2 <= sLen2; ++l2) {
                 if (text1.charAt(l1 - 1) == text2.charAt(l2 - 1)) {
-                    dp[l1][l2] = dp[l1 - 1][l2 - 1] + 1;
+                    dp[newL1][l2] = dp[oldL1][l2 - 1] + 1;
                 } else {
-                    dp[l1][l2] = Math.max(dp[l1 - 1][l2],dp[l1][l2 - 1]);
+                    dp[newL1][l2] = Math.max(dp[oldL1][l2],dp[newL1][l2 - 1]);
                 }
             }
         }
-        return dp[sLen1][sLen2];
+        return dp[sLen1 % 2][sLen2];
     }
 }
