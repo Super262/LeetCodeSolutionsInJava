465a817f9f0649e1fcf4cc27b56cae3ea798a862
Super262 <virusformail@gmail.com>
2021-04-14 10:20:53 +0800

    Finished Problem 341.

---
+++ src/DataStructure/Stack/NestedInteger.java
@@ -0,0 +1,11 @@
+package DataStructure.Stack;
+
+import java.util.List;
+
+public interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<NestedInteger> getList();
+}
