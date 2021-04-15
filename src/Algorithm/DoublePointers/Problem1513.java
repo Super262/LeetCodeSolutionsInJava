40d14349354bc95af8683e5c902434115d8cb93c
Super262 <virusformail@gmail.com>
2021-04-16 00:53:40 +0800

    Finished Problem 1513.

---
+++ src/Algorithm/DoublePointers/Problem1513.java
@@ -0,0 +1,24 @@
+package Algorithm.DoublePointers;
+
+public class Problem1513 {
+    public int numSub(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        final int bound = 1000000007;
+        int answer = 0;
+        int right = 1;
+        for (int left = 0; left < s.length(); ++left) {
+            if (s.charAt(left) != '1') {
+                continue;
+            }
+            right = Math.max(right,left + 1);
+            while (right < s.length() && s.charAt(right) == '1') {
+                ++right;
+            }
+            answer += right - left;
+            answer %= bound;
+        }
+        return answer;
+    }
+}
