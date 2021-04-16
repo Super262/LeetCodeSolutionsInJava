b7b53389d6b7e47ca70ba6ea860a0ceae1a45d01
Super262 <virusformail@gmail.com>
2021-04-16 14:31:49 +0800

    Reformatted code.

--- src/Algorithm/DoublePointers/Problem0160.java
+++ src/Algorithm/DoublePointers/Problem0160.java
@@ -12,10 +12,9 @@ public class Problem0160 {
             p2 = p2.next;
             if (p1 == null && p2 == null) {
                 return null;
-            } else {
-                p1 = p1 == null ? headB : p1;
-                p2 = p2 == null ? headA : p2;
             }
+            p1 = p1 == null ? headB : p1;
+            p2 = p2 == null ? headA : p2;
         }
         return p1;
     }
