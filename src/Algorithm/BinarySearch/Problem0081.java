93b5472737553c493491c6eb78c2e6d73786e150
Super262 <virusformail@gmail.com>
2021-04-28 20:06:59 +0800

    Finished Problem 81.

---
+++ src/Algorithm/BinarySearch/Problem0081.java
@@ -0,0 +1,31 @@
+package Algorithm.BinarySearch;
+
+public class Problem0081 {
+    public boolean search(int[] nums,int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            } else if (nums[mid] > nums[end]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                --end;
+            }
+        }
+        return nums[start] == target || nums[end] == target;
+    }
+}
