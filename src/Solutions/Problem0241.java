9f0f56d376ccd72e0e053824b600da6b17f9197e
fengwei <wuzhongfengwei@163.com>
2021-11-14 18:07:38 -0800

    Finished Problem 241.

--- src/Solutions/Problem0241.java
+++ src/Solutions/Problem0241.java
@@ -1,51 +1,52 @@
 package Solutions;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem0241 {
-    public List<Integer> diffWaysToCompute(String input) {
-        LinkedList<Integer> result = new LinkedList<>();
-        if (input == null) {
-            return result;
-        }
-        int sLen = input.length();
-        int i = 0;
-        while (i < sLen) {
-            if (input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*') {
-                break;
+    public List<Integer> diffWaysToCompute(final String s) {
+        List<String> parts = new ArrayList<>(s.length());
+        for (int i = 0; i < s.length(); ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                int j = i;
+                while (j < s.length() && Character.isDigit(s.charAt(j))) {
+                    ++j;
+                }
+                parts.add(s.substring(i,j));
+                i = j - 1;
+            } else {
+                parts.add(s.substring(i,i + 1));
             }
-            ++i;
         }
-        if (i >= sLen) {
-            result.addLast(Integer.parseInt(input.substring(0,i)));
+        return dfs(parts,0,parts.size() - 1);
+    }
+
+    private List<Integer> dfs(List<String> parts,final int st,final int ed) {
+        List<Integer> result = new ArrayList<>();
+        if (st > ed) {
             return result;
         }
-        while (i < sLen) {
-            if (input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*') {
-                List<Integer> left = diffWaysToCompute(input.substring(0,i));
-                List<Integer> right = diffWaysToCompute(input.substring(i + 1,sLen));
-                if (input.charAt(i) == '+') {
-                    for (int l : left) {
-                        for (int r : right) {
-                            result.addLast(l + r);
-                        }
-                    }
-                } else if (input.charAt(i) == '-') {
-                    for (int l : left) {
-                        for (int r : right) {
-                            result.addLast(l - r);
-                        }
-                    }
-                } else {
-                    for (int l : left) {
-                        for (int r : right) {
-                            result.addLast(l * r);
-                        }
+        if (st == ed) {
+            result.add(Integer.parseInt(parts.get(st)));
+            return result;
+        }
+        for (int i = st; i <= ed; ++i) {
+            if (!parts.get(i).equals("+") && !parts.get(i).equals("-") && !parts.get(i).equals("*")) {
+                continue;
+            }
+            List<Integer> leftPart = dfs(parts,st,i - 1);
+            List<Integer> rightPart = dfs(parts,i + 1,ed);
+            for (int x : leftPart) {
+                for (int y : rightPart) {
+                    if (parts.get(i).equals("+")) {
+                        result.add(x + y);
+                    } else if (parts.get(i).equals("-")) {
+                        result.add(x - y);
+                    } else {
+                        result.add(x * y);
                     }
                 }
             }
-            ++i;
         }
         return result;
     }
