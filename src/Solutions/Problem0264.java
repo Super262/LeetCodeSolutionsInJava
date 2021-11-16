0a35b9a3dd03680c0d072ce76fe35ab656065b9e
fengwei <wuzhongfengwei@163.com>
2021-11-15 23:45:22 -0800

    Finished Problem 264.

--- src/Solutions/Problem0264.java
+++ src/Solutions/Problem0264.java
@@ -1,27 +1,31 @@
 package Solutions;
 
-import java.util.HashSet;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem0264 {
     public int nthUglyNumber(int n) {
-        HashSet<Long> visited = new HashSet<>(n);
-        PriorityQueue<Long> heap = new PriorityQueue<>(n); // Long -> prevents overflow.
-        long[] factors = {2,3,5};
-        visited.add(1L);
-        heap.add(1L);
-        long currentVal = 0;
-        for (int i = 0; i < n; ++i) {
-            currentVal = heap.poll();
-            for (long f : factors) {
-                long nextValue = f * currentVal;
-                if (visited.contains(nextValue)) {
-                    continue;
-                }
-                visited.add(nextValue);
-                heap.add(nextValue);
+        List<Integer> result = new ArrayList<>(n);
+        result.add(1);
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        while (result.size() < n) {
+            int r1 = result.get(p1) * 2;
+            int r2 = result.get(p2) * 3;
+            int r3 = result.get(p3) * 5;
+            int minimal = Math.min(r1,Math.min(r2,r3));
+            result.add(minimal);
+            if (r1 == minimal) {
+                ++p1;
+            }
+            if (r2 == minimal) {
+                ++p2;
+            }
+            if (r3 == minimal) {
+                ++p3;
             }
         }
-        return (int) currentVal;
+        return result.get(n - 1);
     }
 }
